9099adc05d3b3bc50a5dc05fdeadd724d4802f79
CristianCODE123 <starblum123@gmail.com>
2022-04-20 09:25:39 -0500

    first commit

---
+++ model/conexion.php
@@ -0,0 +1,17 @@
+<?php 
+$contrasena = "a1b2c3d4";
+$usuario = "root";
+$nombre_bd = "crud";
+
+try {
+	$bd = new PDO (
+		'mysql:host=localhost;
+		dbname='.$nombre_bd,
+		$usuario,
+		$contrasena,
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+?>
